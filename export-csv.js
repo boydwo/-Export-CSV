28fbaf72a79f66fc1d7340f60ec535bf989952d7
boydwo <marcos.t@ufv.br>
2020-05-09 15:54:10 -0300

    final script

--- export-csv.js
+++ export-csv.js
@@ -6,8 +6,31 @@ const connection = mysql.createConnection({
   database: 'cadastro'
 })
 
-connection.connect((err) => {
-  connection.query('select * from pessoas', (err, results) => {
-    console.log(results)
+const fs = require('fs')
+
+const writable = fs.createWriteStream('pessoas.csv')
+
+writable.write('id, nome', () => {
+
+  connection.connect((err) => {
+    const query = connection.query('select * from pessoas');
+
+    query.on('result', (row) => {
+      // console.log(row)
+      connection.pause()
+      const data = row.id + ', ' + row.nome;
+
+      writable.write('\r\n' + JSON.stringify(data), () => {
+        connection.resume()
+        // console.log('written')
+      })
+    })
+
+    query.on('end', () => {
+      connection.end();
+    })
+    // connection.query('select * from pessoas', (err, results) => {
+    //   console.log(results)
+    // })
   })
 })
